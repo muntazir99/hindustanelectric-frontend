259a8ba4dad606b94da9ab2397f1d91b77d46489
Muntazir Jahangir <muntazir9934@gmail.com>
2025-02-01 16:28:15 +0530

    Implement functionality to add and sell multiple items

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client"; // Updated import for React 18
 import App from "./App.js";
 
 
- // Tailwind CSS styles
  import "./index.css";
 // Create root for React 18
 const root = ReactDOM.createRoot(document.getElementById("root"));
