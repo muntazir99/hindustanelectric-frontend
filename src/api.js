82f891891a2c1c9cc6eb515423806e565ba87fc8
Muntazir Jahangir <muntazir9934@gmail.com>
2025-04-07 09:32:06 +0530

    hsn

--- src/api.js
+++ src/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "https://hindustanelectric.onrender.com",
-  baseURL: "http://localhost:5001",
+  baseURL: "https://hindustanelectric.onrender.com",
+  // baseURL: "http://localhost:5001",
   // base // Update this if the backend URL is different
 });
 
